5cdff2b385fb60d7f8d7abe5ad5a86b3db68c71a
Ransss <joshua.ranin@wvsu.edu.ph>
2025-04-04 16:20:03 +0800

    Commit

--- logIn.php
+++ logIn.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>interLINKED</title>
     <link rel="stylesheet" href="style.css">
-    <script defer src="script.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css'>
 </head>
