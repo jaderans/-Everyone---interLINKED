c74ffab16ebcd658ce08250c946c85cf59896dd9
Ransss <joshua.ranin@wvsu.edu.ph>
2025-04-04 20:21:48 +0800

    Commit

--- logIn.php
+++ logIn.php
@@ -11,10 +11,35 @@
 <body>
 <?php
 ?>
+<div class="container">
+    <div class="content">
+        <h1>Ready to be linked?</h1>
+        <p class="credentials">please enter your credentials</p>
+        <form class="form"  method="POST">
+            <label for="userName"> User name*</label><br>
+            <input type="text" id="userName" name="user" required placeholder="User Name"><br>
 
-<h1> Welcome to interLINKED!</h1>
-<p>testing ba!</p>
-<p style = "color: #1f4c4b; font-weight: lighter">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim exercitationem harum illo illum, maxime quisquam veniam voluptates! Assumenda consectetur, eius exercitationem iste nam quasi similique! Autem commodi nihil numquam praesentium?</p>
+            <label for="userEmail"> Email*</label><br>
+            <input type="text" id="userEmail" name="email" required placeholder="Email"><br>
+
+            <label for="userPass"> Password*</label><br>
+            <input type="password" id="userPass" name="pass" required placeholder="Password"><br>
+
+            <label for="clientType"> Are you a...</label><br>
+            <select name="type" id="clientType">
+                <option value="Client">Client</option>
+                <option value="Freelancer">Freelancer</option>
+                <option value="Admin">Admin</option>
+            </select> <br><br>
+
+            <input type="submit" value="Log In"><br>
+            <input type="submit" value="Sign In">
+        </form>
+    </div>
+</div>
+
+
+
 
 </body>
 </html>
