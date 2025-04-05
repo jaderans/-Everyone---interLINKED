b8de22a4e4c9223883458b004b80349c83d7b2a7
Halo-halo <dohinogjorich23@gmail.com>
2025-04-05 13:28:40 +0800

    - Fixed icons, added dashboard

--- freelancer-navbar-template.php
+++ freelancer-navbar-template.php
@@ -8,7 +8,7 @@
     <link rel="icon" type="image/x-icon" href="imgs/inlFavicon@4x.png">
     <link rel="stylesheet" href="freelancer-nav-style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/font-awesome.min.css">
 
 
     <title>Navigation Template</title>
@@ -20,13 +20,15 @@
     </div>
     <div class="top-right">
         <div class="right-btn">
-            <button class="btn"><a href="logIn.php">Logout</a></button>
+<!--            <button class="btn-top"><a href="frlanceHome.php">Browse</a></button>-->
+            <button class="btn-top"><a href="logIn.php"><i class="fa-solid fa-right-from-bracket"></i> Logout</a></button>
         </div>
         <div class="profile">
-            <img src="imgs/profile.png" alt="">
+            <a href="freelancer-profile-page.php"><img src="imgs/profile.png" alt=""></a>
+
         </div>
         <div class="name">
-            <a href=""><h4 style="font-weight: 700">Furina</h4></a>
+            <a href="freelancer-profile-page.php"><h4 style="font-weight: 700">Furina</h4></a>
             <p style="font-size: 12px">Freelancer</p>
         </div>
     </div>
@@ -35,12 +37,12 @@
     <div class="navbar">
         <div class="sidebar-frame">
             <ul class="side-content">
-                <li><a href="freelancer-home-page.php"><i class="fa-regular fa-house-chimney"></i> Home</a></li>
-                <li><a href="freelancer-project-page.php"><i class="fa-solid fa-chart-simple"></i> Project</a></li>
+                <li><a href="freelancer-dashboard-page.php"><i class="fa-solid fa-database"></i> Dashboard</a></li>
+                <li><a href="freelancer-project-page.php"><i class="fa-solid fa-chart-simple"></i> Projects</a></li>
                 <li><a href="freelancer-salary-page.php"><i class="fa-solid fa-dollar-sign"></i> Salary</a></li>
-                <li><a href="freelancer-notification-page.php"><i class="fa-regular fa-bell"></i> Notification</a></li>
-                <li><a href="freelancer-message-page.php"><i class="fa-regular fa-envelope"></i> Message</a></li>
-                <li><a href="freelancer-profile-page.php"><i class="fa-regular fa-circle-user"></i> Profile</a></li>
+                <li><a href="freelancer-notification-page.php"><i class="fa-solid fa-bell"></i> Notification</a></li>
+                <li><a href="freelancer-message-page.php"><i class="fa-solid fa-envelope"></i> Message</a></li>
+                <li><a href="freelancer-profile-page.php"><i class="fa-solid fa-circle-user"></i> Profile</a></li>
             </ul>
 
             <div class="lower-content">
@@ -48,7 +50,7 @@
 
             </div>
             <div class="help">
-                <h4><a href="#"><i class="fa-regular fa-circle-info"></i> Help & Support</a></h4>
+                <h4><a href="#"><i class="fa-solid fa-circle-info"></i> Help & Support</a></h4>
             </div>
 
         </div>
