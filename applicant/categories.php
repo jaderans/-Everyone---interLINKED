398d528824abf663f6199db8d010d60b2d75ef78
Ransss <joshua.ranin@wvsu.edu.ph>
2025-04-19 04:11:49 +0800

    Commit

---
+++ applicant/categories.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up | INTERLINKED</title>
+    <link rel="icon" href="../imgs/inlFavicon@4x.png">
+    <link rel="stylesheet" href="authenticate.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+<?php
+?>
+
+<div class="topvar">
+    <div class="logo">
+        <img src="../imgs/inl2Logo.png" alt="INTERLINKED Logo">
+    </div>
+</div>
+
+</body>
+</html>
