c02e706adce7a6c9997c506fa5f1574b49b35b2e
Ransss <joshua.ranin@wvsu.edu.ph>
2025-04-20 09:06:22 +0800

    Commit

--- admin/admindash.php
+++ admin/admindash.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>interLINKED</title>
-    <link rel="icon" type="image/x-icon" href="../imgs/inlFavicon@4x.png">
+    <title>Admin | interLINKED</title>
+    <link rel="icon" type="image/x-icon" href="../imgs/inlFaviconwhite.png">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css'>
@@ -12,7 +12,33 @@
 <body>
 <?php
 ?>
+<div class="navbar">
+    <div class="topvar">
+        <!-- Left: Title -->
+        <div class="navtitle">
+            <h1>Admin | <br></h1>
+            <p style="padding-left: 8px">Dashboard</p>
+        </div>
 
-<h1>WELCOME ADMIN!</h1>
+        <!-- Right: Profile -->
+        <div class="navprofile">
+            <div class="name">
+                <h4 style="font-weight: 700">Furina</h4>
+                <p style="font-size: 12px">Admin</p>
+            </div>
+            <div class="profile">
+                <img src="../imgs/profile.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="sidebar">
+    <div class="topvar2">
+        <div class="logo">
+            <img src="../imgs/inl2LogoWhite.png" alt="INTERLINKED Logo">
+        </div>
+    </div>
+</div>
 </body>
 </html>
