ef4accef70992607e1b9a8f1f93790bf20d2330c
Ransss <joshua.ranin@wvsu.edu.ph>
2025-05-17 07:41:00 +0800

    Commit

--- admin/adminMes.php
+++ admin/adminMes.php
@@ -1,52 +1,105 @@
+<?php
+include('interlinkedDB.php');
+
+$conn = connectToDatabase();
+
+// Default search
+$search = $_GET['search'] ?? '';
+
+function fetchUsers($conn, $type, $search = '') {
+    // Determine if we want applicants or non-applicants
+    $typeCondition = $type === 'Applicant' ? "USER_TYPE = 'Applicant'" : "USER_TYPE != 'Applicant'";
+
+    // Start building the query
+    $query = "SELECT * FROM user WHERE $typeCondition";
+
+    // Add search filter if provided
+    if (!empty($search)) {
+        $query .= " AND (USER_FSTNAME LIKE :search OR USER_LSTNAME LIKE :search OR USER_EMAIL LIKE :search)";
+    }
+
+    $stmt = $conn->prepare($query);
+
+    // Bind parameters
+    if (!empty($search)) {
+        $stmt->execute(['search' => "%$search%"]);
+    } else {
+        $stmt->execute();
+    }
+
+    return $stmt;
+}
+
+
+
+$selectedUser = null;
+
+if (isset($_GET['id'])) {
+    $stmt = $conn->prepare("SELECT * FROM user WHERE USER_ID = ?");
+    $stmt->execute([$_GET['id']]);
+    $selectedUser = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+$usersResult = fetchUsers($conn, 'User', $search);
+$applicantsResult = fetchUsers($conn, 'Applicant', $search);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin | interLINKED</title>
+    <link rel="stylesheet" href="admin.css">
     <link rel="icon" type="image/x-icon" href="../imgs/inlFaviconwhite.png">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
-<?php
-?>
-<div class="navbar">
-    <div class="topvar">
-        <div class="navtitle">
-            <h1>Admin | <br></h1>
-            <p style="padding-left: 8px">Message</p>
-        </div>
-
-        <div class="navprofile">
-            <div class="name">
-                <h4 style="font-weight: 700">Furina</h4>
-                <p style="font-size: 12px">Admin</p>
-            </div>
-            <div class="profile">
-                <img src="../imgs/profile.png" alt="">
-            </div>
-        </div>
-    </div>
-</div>
-
+<!-- Sidebar -->
 <div class="sidebar">
     <div class="topvar2">
         <div class="logo">
-            <img src="../imgs/inl2LogoWhite.png" alt="INTERLINKED Logo">
+            <img src="../../imgs/inl2LogoWhite.png" alt="Logo">
         </div>
     </div>
-    <div class="navbuttons">
-        <ul class="side-content">
-            <li><a href="adminDash.php"><i class="fas fa-database"></i> Dashboard</a></li>
-            <li><a href="adminProj.php"><i class="fas fa-project-diagram"></i> Projects</a></li>
-            <li><a href="adminPay.php"><i class="fas fa-dollar-sign"></i> Salary</a></li>
-            <li><a href="adminUser.php"><i class="fas fa-users"></i> Users</a></li>
-            <li><a href="adminMes.php"><i class="fas fa-envelope"></i> Message</a></li>
-            <li><a href="adminProf.php"><i class="fas fa-user"></i> Profile</a></li>
-        </ul>
+    <ul class="side-content">
+        <li><a href="adminDash.php"><i class="fas fa-database"></i> Dashboard</a></li>
+        <li><a href="adminProj.php"><i class="fas fa-project-diagram"></i> Projects</a></li>
+        <li><a href="adminPay.php"><i class="fas fa-dollar-sign"></i> Salary</a></li>
+        <li><a href="adminUser.php"><i class="fas fa-users"></i> Users</a></li>
+        <li><a href="adminUser.php"><i class="fas fa-bell"></i> Notifications</a></li>
+        <li><a href="adminMes.php"><i class="fas fa-envelope"></i> Message</a></li>
+        <li><a href="adminProf.php"><i class="fas fa-user"></i> Profile</a></li>
+        <div class="btm-content">
+            <button class="logout-button" onclick="window.location.href='../loginSignup/logIn.php';">
+                <i class="fas fa-sign-out"></i> Log Out
+            </button>
+        </div>
+    </ul>
+</div>
+
+<!-- Navbar -->
+<div class="navbar">
+    <div class="topvar">
+        <div class="navtitle">
+            <h1>Admin | </h1>
+            <p>MESSAGES</p>
+        </div>
+        <div class="navprofile">
+            <div class="name">
+                <h4>Ian Harvey Yap</h4>
+            </div>
+            <div class="profile">
+                <img src="../../imgs/profile.png" alt="Admin Profile">
+            </div>
+        </div>
     </div>
 </div>
+</div>
+<script>
+</script>
 </body>
 </html>
