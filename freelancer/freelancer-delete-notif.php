b7bbde2cbcc2fee992f9bb5675af3ef463fb291d
Halo-halo <dohinogjorich23@gmail.com>
2025-05-17 15:15:58 +0800

    Master slave implementation and UI fix

---
+++ freelancer/freelancer-delete-notif.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include_once 'interlinkedDB.php';
+$master_con = connectToDatabase(3306);
+$slave_con = connectToDatabase(3307);
+
+$id = $_POST['notif_Id'];
+echo $id;
+
+$stmt = $master_con->prepare("DELETE FROM notifications WHERE NOTIF_ID = :id");
+$stmt->bindParam(':id', $id);
+$result = $stmt->execute();
+
+if ($result) {
+    header("Location: freelancer-notification-page.php");
+    exit();
+} else {
+    echo "Error deleting record";
+}
+
+
+
