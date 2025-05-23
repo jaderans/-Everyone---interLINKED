668531ac5d1aa7aff21ab89f5b9a6fe01d4ef9a8
Halo-halo <dohinogjorich23@gmail.com>
2025-05-23 20:27:27 +0800

    Message in admin

---
+++ admin/search.php
@@ -0,0 +1,27 @@
+<?php
+include_once 'interlinkedDB.php';
+$master_con = connectToDatabase(3006);
+$slave_con = connectToDatabase(3007);
+
+$name = $_GET['keyword'];
+
+$stmt = $slave_con->prepare("
+    SELECT * FROM `user` 
+    WHERE (`USER_NAME` LIKE :name OR `USER_TYPE` LIKE :name)
+    AND `USER_TYPE` <> 'Applicant'
+");
+$stmt->execute(['name' => "%$name%"]);
+$result = $stmt->fetchAll();
+
+
+if(empty($result)){
+    echo "User not found!";
+}
+
+ob_start();
+foreach ($result as $data) {
+    $name = $data['USER_NAME'];
+    $type = $data['USER_TYPE'];
+    include 'result.php';
+    echo ob_get_clean();
+}
